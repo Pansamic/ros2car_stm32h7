fff3d67574a6dd8834b443ce88da9eeceb177de4
Pansamic <pansamic@foxmail.com>
2025-03-25 19:49:55 +0800

    perf:Decrease memory occupation of syslog.

--- Core/Src/syslog.c
+++ Core/Src/syslog.c
@@ -4,101 +4,129 @@
 #include "lwprintf.h"
 
 // one more byte for string end check.
-char log_fmt_buffer1[LOG_TEMP_BUFFER_SIZE+1];
-char log_fmt_buffer2[2*LOG_TEMP_BUFFER_SIZE+1];
+char log_fmt_buffer[LOG_TEMP_BUFFER_SIZE];
 
 void log_critical(const char* fmt, ...)
 {
 #if LOG_LEVEL <= LOG_LEVEL_CRITICAL
+    const char* p = log_fmt_buffer;
     va_list args;
     va_start(args, fmt);
     size_t len = 0;
-    lwvsnprintf(log_fmt_buffer1, LOG_TEMP_BUFFER_SIZE, fmt, args);
+    uint64_t nsec = (uint64_t)ETH->MACSTNR;
+    nsec *= 999999999ULL;
+    nsec /= 0x7FFFFFFFULL;
+    p += lwsnprintf(p, LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[31mcritical\033[0m]", ETH->MACSTSR, (uint32_t)nsec/1000);
     va_end(args);
-    uint64_t nsec = (uint32_t)((uint64_t)ETH->MACSTNR*999999999ULL/0x7FFFFFFFULL);
-    lwsnprintf(log_fmt_buffer2, 2*LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[31mcritical\033[0m]%s", ETH->MACSTSR, (uint32_t)nsec/1000, log_fmt_buffer1);
-    len = strlen(log_fmt_buffer2);
-    uart_send(&uart1_cb, log_fmt_buffer2, len);
+    len = p-log_fmt_buffer;
+    p += lwvsnprintf(p, LOG_TEMP_BUFFER_SIZE-len, fmt, args);
+    len = p-log_fmt_buffer;
+    uart_send(&uart1_cb, log_fmt_buffer, len);
 #else
     (void)fmt;
 #endif
 }
+
 void log_error(const char* fmt, ...)
 {
 #if LOG_LEVEL <= LOG_LEVEL_ERROR
+    const char* p = log_fmt_buffer;
     va_list args;
     va_start(args, fmt);
     size_t len = 0;
-    lwvsnprintf(log_fmt_buffer1, LOG_TEMP_BUFFER_SIZE, fmt, args);
+    uint64_t nsec = (uint64_t)ETH->MACSTNR;
+    nsec *= 999999999ULL;
+    nsec /= 0x7FFFFFFFULL;
+    p += lwsnprintf(p, LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[31m  error \033[0m]", ETH->MACSTSR, (uint32_t)nsec/1000);
     va_end(args);
-    uint64_t nsec = (uint32_t)((uint64_t)ETH->MACSTNR*999999999ULL/0x7FFFFFFFULL);
-    lwsnprintf(log_fmt_buffer2, 2*LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[31m  error \033[0m]%s", ETH->MACSTSR, (uint32_t)nsec/1000, log_fmt_buffer1);
-    len = strlen(log_fmt_buffer2);
-    uart_send(&uart1_cb, log_fmt_buffer2, len);
+    len = p-log_fmt_buffer;
+    p += lwvsnprintf(p, LOG_TEMP_BUFFER_SIZE-len, fmt, args);
+    len = p-log_fmt_buffer;
+    uart_send(&uart1_cb, log_fmt_buffer, len);
 #else
     (void)fmt;
 #endif
 }
+
 void log_warn(const char* fmt, ...)
 {
 #if LOG_LEVEL <= LOG_LEVEL_WARN
+    const char* p = log_fmt_buffer;
     va_list args;
     va_start(args, fmt);
     size_t len = 0;
-    lwvsnprintf(log_fmt_buffer1, LOG_TEMP_BUFFER_SIZE, fmt, args);
+    uint64_t nsec = (uint64_t)ETH->MACSTNR;
+    nsec *= 999999999ULL;
+    nsec /= 0x7FFFFFFFULL;
+    p += lwsnprintf(p, LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[33m  warn  \033[0m]", ETH->MACSTSR, (uint32_t)nsec/1000);
     va_end(args);
-    uint64_t nsec = (uint32_t)((uint64_t)ETH->MACSTNR*999999999ULL/0x7FFFFFFFULL);
-    lwsnprintf(log_fmt_buffer2, 2*LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[33m  warn  \033[0m]%s", ETH->MACSTSR, (uint32_t)nsec/1000, log_fmt_buffer1);
-    len = strlen(log_fmt_buffer2);
-    uart_send(&uart1_cb, log_fmt_buffer2, len);
+    len = p-log_fmt_buffer;
+    p += lwvsnprintf(p, LOG_TEMP_BUFFER_SIZE-len, fmt, args);
+    len = p-log_fmt_buffer;
+    uart_send(&uart1_cb, log_fmt_buffer, len);
 #else
     (void)fmt;
 #endif
 }
+
 void log_info(const char* fmt, ...)
 {
 #if LOG_LEVEL <= LOG_LEVEL_INFO
+    const char* p = log_fmt_buffer;
     va_list args;
     va_start(args, fmt);
     size_t len = 0;
-    lwvsnprintf(log_fmt_buffer1, LOG_TEMP_BUFFER_SIZE, fmt, args);
+    uint64_t nsec = (uint64_t)ETH->MACSTNR;
+    nsec *= 999999999ULL;
+    nsec /= 0x7FFFFFFFULL;
+    p += lwsnprintf(p, LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[34m  info  \033[0m]", ETH->MACSTSR, (uint32_t)nsec/1000);
     va_end(args);
-    uint64_t nsec = (uint32_t)((uint64_t)ETH->MACSTNR*999999999ULL/0x7FFFFFFFULL);
-    lwsnprintf(log_fmt_buffer2, 2*LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[34m  info  \033[0m]%s", ETH->MACSTSR, (uint32_t)nsec/1000, log_fmt_buffer1);
-    len = strlen(log_fmt_buffer2);
-    uart_send(&uart1_cb, log_fmt_buffer2, len);
+    len = p-log_fmt_buffer;
+    p += lwvsnprintf(p, LOG_TEMP_BUFFER_SIZE-len, fmt, args);
+    len = p-log_fmt_buffer;
+    uart_send(&uart1_cb, log_fmt_buffer, len);
 #else
     (void)fmt;
 #endif
 }
+
 void log_debug(const char* fmt, ...)
 {
 #if LOG_LEVEL <= LOG_LEVEL_DEBUG
+    const char* p = log_fmt_buffer;
     va_list args;
     va_start(args, fmt);
     size_t len = 0;
-    lwvsnprintf(log_fmt_buffer1, LOG_TEMP_BUFFER_SIZE, fmt, args);
+    uint64_t nsec = (uint64_t)ETH->MACSTNR;
+    nsec *= 999999999ULL;
+    nsec /= 0x7FFFFFFFULL;
+    p += lwsnprintf(p, LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[36m  debug \033[0m]", ETH->MACSTSR, (uint32_t)nsec/1000);
     va_end(args);
-    uint64_t nsec = (uint32_t)((uint64_t)ETH->MACSTNR*999999999ULL/0x7FFFFFFFULL);
-    lwsnprintf(log_fmt_buffer2, 2*LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[36m  debug \033[0m]%s", ETH->MACSTSR, (uint32_t)nsec/1000, log_fmt_buffer1);
-    len = strlen(log_fmt_buffer2);
-    uart_send(&uart1_cb, log_fmt_buffer2, len);
+    len = p-log_fmt_buffer;
+    p += lwvsnprintf(p, LOG_TEMP_BUFFER_SIZE-len, fmt, args);
+    len = p-log_fmt_buffer;
+    uart_send(&uart1_cb, log_fmt_buffer, len);
 #else
     (void)fmt;
 #endif
 }
+
 void log_trace(const char* fmt, ...)
 {
 #if LOG_LEVEL <= LOG_LEVEL_TRACE
+    const char* p = log_fmt_buffer;
     va_list args;
     va_start(args, fmt);
     size_t len = 0;
-    lwvsnprintf(log_fmt_buffer1, LOG_TEMP_BUFFER_SIZE, fmt, args);
+    uint64_t nsec = (uint64_t)ETH->MACSTNR;
+    nsec *= 999999999ULL;
+    nsec /= 0x7FFFFFFFULL;
+    p += lwsnprintf(p, LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[36m  trace \033[0m]", ETH->MACSTSR, (uint32_t)nsec/1000);
     va_end(args);
-    uint64_t nsec = (uint32_t)((uint64_t)ETH->MACSTNR*999999999ULL/0x7FFFFFFFULL);
-    lwsnprintf(log_fmt_buffer2, 2*LOG_TEMP_BUFFER_SIZE, "[%10lu.%06lu][\033[36m  trace \033[0m]%s", ETH->MACSTSR, (uint32_t)nsec/1000, log_fmt_buffer1);
-    len = strlen(log_fmt_buffer2);
-    uart_send(&uart1_cb, log_fmt_buffer2, len);
+    len = p-log_fmt_buffer;
+    p += lwvsnprintf(p, LOG_TEMP_BUFFER_SIZE-len, fmt, args);
+    len = p-log_fmt_buffer;
+    uart_send(&uart1_cb, log_fmt_buffer, len);
 #else
     (void)fmt;
 #endif
